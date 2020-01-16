5b856dbc34fe8550e72cb96d97384e6903732a8e
Daniel Stoklosa <morormo@gmail.com>
2020-01-16 15:29:05 +0100

    ADD Column

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,9 +7,15 @@ class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className= {styles.subtitle}>Hello World</h2>
-        <List title='Thinks to do' />
+        <h1 className={styles.title}>
+          My first React app
+        </h1>
+        <h2 className= {styles.subtitle}>
+          Hello World
+        </h2>
+        <List title={['Things to do ', <sup key="1">soon!</sup>]} picture={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"}>
+          
+        </List>
       </main>
     )
   }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
 
-class Hero extends React.Component {
-  render() {
-    return (
-      <header className={styles.component}>
-        <h2 className={styles.title}>Things to do</h2>
-        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
-      </header>
-    )
-  }
-}
+const Hero = props => (
+  <header className={styles.component}>
+    <h2 className={styles.title}>
+      {props.titleText}
+    </h2>
+    <img className={styles.image} src={props.pictureSrc} />
+  </header>
+);
+
+Hero.propTypes = {
+  titleText: PropTypes.node.isRequired,
+  pictureSrc: PropTypes.string,
+};
+
 
 export default Hero;
