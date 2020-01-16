d81718347de116ccae22d2ea4d7f171267f1d48e
Daniel Stoklosa <morormo@gmail.com>
2020-01-16 11:56:59 +0100

    Fix Hero

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
-import Hero from '../Hero/Hero.js';
+
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
-        <Hero />
+        <h2 className= {styles.subtitle}>Hello World</h2>
         <List />
       </main>
     )
