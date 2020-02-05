ecd581ee9027631f516912787cd56f8390fdf6a3
Daniel Stoklosa <morormo@gmail.com>
2020-02-05 09:08:41 +0100

    Fix bug, end of task

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 import Column from './Column';
-import getCardsForColumn from '../../redux/cardsRedux';
-import createActionAddCard from '../../redux/cardsRedux';
+import {getCardsForColumn, createActionAddCard} from '../../redux/cardsRedux';
+
 
 const mapStateToProps = (state, props) => ({
   cards: getCardsForColumn(state, props.id),
